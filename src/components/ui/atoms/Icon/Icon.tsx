5314cfe8483d71e7c11a1b91e2d239e43df27a69
ElderDC <eldergabrieldcr@gmail.com>
2022-10-14 21:06:38 -0400

    Fix symbol type

--- src/components/ui/atoms/Icon/Icon.tsx
+++ src/components/ui/atoms/Icon/Icon.tsx
@@ -12,7 +12,7 @@ const Icon = React.forwardRef<any, IconProps>(function Icon(props, ref) {
 	const { children, className, style, symbol } = props
 	const iconClass = classNames(className, {
 		'material-icons': !symbol,
-		'material-symbols': symbol,
+		'material-symbols-outlined': symbol,
 	})
 
 	return (
