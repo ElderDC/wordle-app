30f0c4771db38234e08989b366a6c8c19aa4d87a
ElderDC <eldergabrieldcr@gmail.com>
2022-10-15 23:43:28 -0400

    wordle service added

---
+++ src/services/wordle.service.ts
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const WORDLE_API_URL = import.meta.env.VITE_WORDLE_API_URL || 'https://thatwordleapi.azurewebsites.net'
+
+const wordleAPI = axios.create({
+	baseURL: WORDLE_API_URL,
+	timeout: 1000,
+})
+
+export const getWordService = async () => {
+	return wordleAPI.get('/get')
+}
+
+export const askWordService = async (word: string) => {
+	return wordleAPI.get('/ask', {
+		params: { word },
+	})
+}
