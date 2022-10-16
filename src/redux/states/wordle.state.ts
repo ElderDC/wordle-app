04f653c2d8cfb1c8f43a67193ed42756f19eb73d
ElderDC <eldergabrieldcr@gmail.com>
2022-10-15 23:50:37 -0400

    Property firstTime added to wordle state

--- src/redux/states/wordle.state.ts
+++ src/redux/states/wordle.state.ts
@@ -6,14 +6,16 @@ export interface WordleState {
     word: string
     tries: string[]
     lastTime: number
+    firstTime: number
     totalTries: number
     successTries: number
 }
 
 export const wordleInitialState: WordleState = {
-    word: 'manta',
+    word: '',
     tries: [],
     lastTime: 0,
+    firstTime: 0,
     totalTries: 0,
     successTries: 0,
 }
@@ -25,6 +27,7 @@ export const wordleSlice = createSlice({
         setWord: (state, action) => ({ ...state, word: action.payload }),
         setTries: (state, action) => ({ ...state, tries: action.payload }),
         setLastTime: (state, action) => ({ ...state, lastTime: action.payload }),
+        setFirstTime: (state, action) => ({ ...state, firstTime: action.payload }),
         setTotalTries: (state, action) => ({ ...state, totalTries: action.payload }),
         setSuccessTries: (state, action) => ({ ...state, successTries: action.payload }),
         resetWordleState: () => wordleInitialState,
@@ -35,6 +38,7 @@ export const {
     setWord,
     setTries,
     setLastTime,
+    setFirstTime,
     setTotalTries,
     setSuccessTries,
     resetWordleState,
